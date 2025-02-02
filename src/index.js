ca41cd818a72ae76244ae3fc6a94d0177ee04f52
sourinry <144391871+sourinry@users.noreply.github.com>
2025-02-02 17:45:49 +0530

    utils files addeds

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // require('dotenv').config({path: './env});
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+    path: './env'
+});
 
 import connectDB  from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(`ERROR: `, error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(`MONGODB connecte failed: ${error}`);
+})
